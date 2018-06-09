c5d135ef7bc7b2cbad17e8736f6f6abdf6149ba6
fouquetc <38565621+fouquetc@users.noreply.github.com>
2018-06-09 22:15:50 +0200

    Update monJS.js

--- js/monJS.js
+++ js/monJS.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
         $("#mainAchievements li").css({"font-weight":"normal", "color":"black"});                 
         $("#mainActivities li").css({"font-weight":"normal", "color":"unset"});                 
         $("#technicalSkills li").css({"font-weight":"normal", "color":"black"});                 
-        $(this).css({"color":"rgb(0,64,255)","font-weight":"bold"});
+        $(this).css({"color":"rgb(0,128,255)","font-weight":"bold"});
         $("#achievementDetails #Title").html($(this).find(".Title").html());
         $("#achievementDetails #Detail").html($(this).find(".Detail").html());
 
