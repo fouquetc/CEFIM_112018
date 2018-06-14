c1998b523cc206e11353f6f2a06d0f31e7aef4a6
fouquetc <38565621+fouquetc@users.noreply.github.com>
2018-06-14 19:22:53 +0200

    Update monJS.js

--- js/monJS.js
+++ js/monJS.js
@@ -1,5 +1,6 @@
-
 $(document).ready(function(){
+
+    // BEGIN ACHIEVEMENTS DETAIL    
     var myClass
     var myPreviousClass
     $(".achievements li").click(function(){
@@ -23,13 +24,55 @@ $(document).ready(function(){
             $("#achievementDetails").animate({left: myFinalPos}, "fast");
         }
         myPreviousClass = myClass;
-        $("#mainAchievements li").css({"font-weight":"normal", "color":"black"});                 
+        $("#mainAchievements li").css({"font-weight":"normal", "color":"unset"});                 
         $("#mainActivities li").css({"font-weight":"normal", "color":"unset"});                 
-        $("#technicalSkills li").css({"font-weight":"normal", "color":"black"});                 
+        $("#technicalSkills li").css({"font-weight":"normal", "color":"unset"});                 
         $(this).css({"color":"rgb(0,128,255)","font-weight":"bold"});
         $("#achievementDetails #Title").html($(this).find(".Title").html());
         $("#achievementDetails #Detail").html($(this).find(".Detail").html());
 
-    }); 
+    });
+    // END ACHIEVEMENTS DETAIL      
+
+    currentSlide(1);
+    var slideIndex = 1;
+    showSlides(slideIndex);
+   
 
 });
+
+    // BEGIN SLIDESHOW 
+
+    // Boutons Next/previous
+    function plusSlides(n) {
+    showSlides(slideIndex += n);
+    }
+
+    // Gestion des Thumbnails 
+    function currentSlide(n) {
+    showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+    var i;
+    var slides = $(".slideshowImage");
+    var dots = $(".slideshowThumbnail");
+    var captionText = $("description");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+    } 
+
+    // END SLIDESHOW  
+
+
+
+  
